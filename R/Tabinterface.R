2580b1ad06d8b6ec7a1be1fb4ca1cc5af0dda10c
Tadeáš Staněk <91667673+stanek-tadeas@users.noreply.github.com>
2025-05-30 17:27:18 +0200

    Deleted log

--- R/Tabinterface.R
+++ R/Tabinterface.R
@@ -162,9 +162,6 @@ moduleTabInterface_Server <- function(id, language, data = dataDENTRO, interface
         reactive_inputs(allinputs)   # we need to get to the used inputs in downloadhandler
         reformated<-character()
         if(length(allinputs)>0) {
-          if (AllowDebug) {
-            Debugging(paste0("### allinputs:"), str(allinputs))
-          }
           # List of things (chr for selectInput, 
           #                 chr for radioButtons
           #                 chr vector for checkboxGroupInput,
